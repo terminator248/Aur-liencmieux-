7460e08f1dc1d0c9fed8319b09a1875e95469e7d
Julien BAURECHE <julien.baureche@edu.esiee.fr>
2024-10-18 17:35:32 +0200

    J'ai rajouté un bouton : dans activities_main, j'ai défini le bouton et dans le main activities j'ai implémenter le bouton et quand tu cliques dessus, ça te dit que t'as cliquer (possibilité de changer ce qu'il fait par la suite)

--- hello_world/app/src/main/java/com/example/helloworld/MainActivity.java
+++ hello_world/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        // Référence du TextView
+        TextView textView = findViewById(R.id.textView);
+
+        // Référence du bouton
+        Button button = findViewById(R.id.button);
+
+        // Définir un OnClickListener sur le bouton
+        button.setOnClickListener(v -> textView.setText("Bouton cliqué !"));
+    }  // Ici on ferme la méthode onCreate()
+
+}  // Ici on ferme la classe MainActivity
